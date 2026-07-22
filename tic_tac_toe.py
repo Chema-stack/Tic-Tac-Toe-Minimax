147f406fec7fc9babc59572eea57d0c1a346e957
Chema-stack <josemaria.romero4@alu.uclm.es>
2026-07-22 13:04:05 +0200

    Implementacion funcion movimiento y constructor

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,8 +1,21 @@
 class Juego:
-    tablero = [["-" for _ in range(3)] for _ in range(3)]
 
-    def normalizador(fila,columna): #Esta funcion normaliza los movimientos
+    def __init__(self):
+        self.tablero = [["-" for _ in range(3)] for _ in range(3)]
+
+    def normalizador(self, fila,columna): #Esta funcion normaliza los movimientos
         return fila - 1, columna - 1 #Le quita a la fila y a la columna uno
                                     #para que entre en los limites de la matriz
 
+    def movimiento(self,jugador,fila,columna):
+        
+        fila, columna = self.normalizador(fila,columna)
+
+        if self.tablero[fila][columna] == "-":
+            self.tablero[fila][columna] = jugador
+        else:
+            raise ValueError("Jugada no valida")
+        
     
+
+            
