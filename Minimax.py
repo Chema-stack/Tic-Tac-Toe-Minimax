9d48405e07c7c8aac910815d9ac80415f6d230f5
Chema-stack <josemaria.romero4@alu.uclm.es>
2026-07-24 13:56:17 +0200

    Esqueleto algoritmo Minimax

--- Minimax.py
+++ Minimax.py
@@ -1,2 +1,31 @@
+import tic_tac_toe
 class Minimax:
-    
+
+    def __init__(self):
+        self.juego = tic_tac_toe()
+
+    def algoritmo(self,tablero,jugador_max):
+
+        if self.juego.comprobar('x'):
+            return -10
+        elif self.juego.comprobar('o'):
+            return 10
+        elif self.juego.jugadas == 9:
+            return 0
+
+        
+        if jugador_max:
+            evaluacion_maxima = float('-inf')
+            for jugada in tablero:
+                evaluacion = self.algoritmo(jugada,False)
+                evaluacion_maxima = max(evaluacion_maxima,evaluacion)
+            return evaluacion_maxima
+        else:
+            evaluacion_minima = float('inf')
+            for jugada in tablero:
+                evaluacion = self.algoritmo(jugada,True)
+                evaluacion_minima = min(evaluacion_minima,evaluacion)
+            return evaluacion_minima
+        
+
+        
